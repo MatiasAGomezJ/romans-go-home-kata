6cd5c35f1d7182e5b6bb0bee65332a659b3804fe
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-03-23 20:18:02 +0100

    Initial commit

---
+++ src/test/java/edu/poniperro/romansgohome/AppTest.java
@@ -0,0 +1,12 @@
+package edu.poniperro.romansgohome;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
